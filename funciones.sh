3d5b35983b88b5acf75582ceda978dcd4fd16f8f
tincho12 <tincho12@gmail.com>
2013-10-25 17:33:31 -0300

    Arregle todos los errores de emi

--- funciones.sh
+++ funciones.sh
@@ -137,9 +137,14 @@ function vmStatus() {
 
 			tmp=$(echo $name '|' $state '|' $port '|' $drbd '|' $mount '|' $maybeVmId)
 			if [ "$aListStatus" != "" ]; then
-	                        echo $tmp | grep -e $(echo $aListStatus |sed 's/ / -e /g') | awk -F'|' '{ printf "\033[1;30m%20s \033[1;35m%-17s \033[1;30m%5d\n", $1, $2, $3}'
+	                        echo $tmp | grep -e $(echo $aListStatus |sed 's/ / -e /g') | awk -F'|' '{ printf "\033[1;30m%-20s \033[1;35m%-17s \033[1;30m%5d\n", $1, $2, $3}'
 			else
-				echo $tmp | awk -F'|' '{ printf "\033[1;30m%-20s \033[1;35m%-17s \033[1;30m%5d \033[1;34m%-15s \033[1;33m%-s \033[1;31m%s \n", $1,$2, $3, $4, $5, $6}'
+				if [ "$name" != '"<inaccessible>"' ]; then
+					echo $tmp | awk -F'|' '{ printf "\033[1;30m%-20s \033[1;35m%-17s \033[1;30m%5d \033[1;34m%-15s \033[1;33m%-50s \033[1;31m%s \n", $1,$2, $3, $4, $5, $6}'
+				else
+					echo $tmp | awk -F'|' '{ printf "\033[1;30m%-20s \033[1;31m%-s\n", $1,$6}'
+				fi
+
 			fi
 
                 done
